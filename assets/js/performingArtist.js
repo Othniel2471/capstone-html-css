3ad3d9cbaceff638e97e36058c28f673acee73ba
othniel2471 <othnielomaidu@gmail.com>
2023-05-22 22:07:23 +0100

    designed the desktop version

--- assets/js/performingArtist.js
+++ assets/js/performingArtist.js
@@ -2,7 +2,7 @@ const artist = [
   {
     dotImg: 'assets/images/pictures/dots.png',
     artistImg: 'assets/images/pictures/johnSmith.png',
-    artistName: 'JOhn Smith',
+    artistName: 'John Smith',
     performanceTime: 'Friday, June 10th at 7:00 PM',
     artistDecs: 'John Smith is a singer-songwriter from Nashville, Tennessee known for his soulful voice and heartfelt lyrics. He has two albums under his belt and is excited to share his music with a new audience.',
   },
